2a494d697568215bc7dd0f197277944f318f6fc2
thsaravana <th.saravana@gmail.com>
2019-08-03 23:46:07 +0530

    Showing margin icon for Subscribe for base use case

--- src/main/kotlin/com/madrapps/eventbus/subscribe/SubscribeLineMarkerProvider.kt
+++ src/main/kotlin/com/madrapps/eventbus/subscribe/SubscribeLineMarkerProvider.kt
@@ -3,29 +3,36 @@ package com.madrapps.eventbus.subscribe
 import com.intellij.codeInsight.daemon.LineMarkerInfo
 import com.intellij.codeInsight.daemon.LineMarkerProvider
 import com.intellij.icons.AllIcons
-import com.intellij.openapi.editor.markup.GutterIconRenderer
-import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.openapi.editor.markup.GutterIconRenderer.Alignment.RIGHT
 import com.intellij.psi.PsiElement
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.toUElement
 
 class SubscribeLineMarkerProvider : LineMarkerProvider {
 
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
-        println(element)
+        val uElement = element.toUElement()
+        if (uElement is UMethod) {
+            val annotation = uElement.annotations.find { it.qualifiedName == "org.greenrobot.eventbus.Subscribe" }
+            if (annotation != null) {
+                val psiElement = uElement.uastAnchor?.sourcePsi
+                if (psiElement != null) {
+                    return SubscribeLineMarkerInfo(psiElement, "post for ---")
+                }
+            }
+        }
         return null
     }
 }
 
 private class SubscribeLineMarkerInfo(
     private val psiElement: PsiElement,
-    private val message: String,
-    private val file: VirtualFile
+    private val message: String
 ) : LineMarkerInfo<PsiElement>(
     psiElement,
     psiElement.textRange,
     AllIcons.Icons.Ide.NextStep,
     { message },
     null,
-    GutterIconRenderer.Alignment.RIGHT
-) {
-
-}
+    RIGHT
+)
