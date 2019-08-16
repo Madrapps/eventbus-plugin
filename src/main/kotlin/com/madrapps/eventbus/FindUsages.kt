42df862acd92fd34516b75045795148a1d26ec36
thsaravana <th.saravana@gmail.com>
2019-08-16 18:26:10 +0530

    Pushing changes to 2018.3 branch #6

--- src/main/kotlin/com/madrapps/eventbus/FindUsages.kt
+++ src/main/kotlin/com/madrapps/eventbus/FindUsages.kt
@@ -5,9 +5,8 @@ import com.intellij.psi.search.searches.ReferencesSearch
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.Usage
 import com.intellij.usages.UsageInfo2UsageAdapter
-import org.jetbrains.uast.UElement
-import org.jetbrains.uast.getParentOfType
-import org.jetbrains.uast.toUElement
+import org.jetbrains.uast.*
+import org.jetbrains.uast.UastCallKind.Companion.METHOD_CALL
 
 internal fun search(element: PsiElement): Collection<UsageInfo> {
     val references = ReferencesSearch.search(element).findAll()
@@ -20,4 +19,28 @@ internal fun Usage.toUElement(): UElement? {
 
 internal inline fun <reified T : UElement> Usage.getType(): T? {
     return toUElement()?.getParentOfType<T>()
+}
+
+internal fun UElement.getCallExpression(): UCallExpression? {
+    if (this is UCallExpression) {
+        if (getParentOfType<UQualifiedReferenceExpression>() == null) {
+            return this
+        }
+    } else if (this is UQualifiedReferenceExpression) {
+        return selector as? UCallExpression
+    }
+    return null
+}
+
+internal fun UElement.getParentOfTypeCallExpression(): UCallExpression? {
+    val find = withContainingElements
+        .filterIsInstance<UCallExpression>()
+        .find {
+            it.kind == METHOD_CALL && it.getParentOfType<UQualifiedReferenceExpression>() == null
+        }
+    val uCallExpression = withContainingElements
+        .filterIsInstance<UQualifiedReferenceExpression>()
+        .firstOrNull()
+        ?.selector as? UCallExpression
+    return find ?: uCallExpression
 }
--- src/main/kotlin/com/madrapps/eventbus/post/PostLineMarkerProvider.kt
+++ src/main/kotlin/com/madrapps/eventbus/post/PostLineMarkerProvider.kt
@@ -11,6 +11,8 @@ import com.intellij.psi.impl.source.PsiClassReferenceType
 import com.intellij.ui.awt.RelativePoint
 import com.intellij.usageView.UsageInfo
 import com.intellij.usages.UsageInfo2UsageAdapter
+import com.madrapps.eventbus.getCallExpression
+import com.madrapps.eventbus.getParentOfTypeCallExpression
 import com.madrapps.eventbus.search
 import com.madrapps.eventbus.showPostUsages
 import com.madrapps.eventbus.subscribe.isSubscribe
@@ -26,8 +28,8 @@ class PostLineMarkerProvider : LineMarkerProvider {
     override fun getLineMarkerInfo(element: PsiElement): LineMarkerInfo<*>? {
         val uElement = element.toUElement() ?: return null
         if (element !is PsiExpressionStatement) {
-            val uCallExpression = uElement.getPostCallExpression()
-            if (uCallExpression != null) {
+            val uCallExpression = uElement.getCallExpression()
+            if (uCallExpression != null && uCallExpression.isPost()) {
                 val psiIdentifier = uCallExpression.methodIdentifier?.sourcePsi ?: return null
                 return PostLineMarkerInfo(psiIdentifier, uCallExpression)
             }
@@ -40,22 +42,15 @@ internal fun UsageInfo.isPost(): Boolean {
     val uElement = element.toUElement()
     if (uElement != null) {
         if (uElement.getParentOfType<UImportStatement>() == null) {
-            return uElement.getParentOfType<UQualifiedReferenceExpression>()?.getPostCallExpression() != null
+            return uElement.getParentOfTypeCallExpression()?.isPost() == true
         }
     }
     return false
 }
 
-private fun UElement.getPostCallExpression(): UCallExpression? {
-    if (this is UQualifiedReferenceExpression) {
-        val uCallExpression = selector as? UCallExpression ?: return null
-        if (uCallExpression.receiverType?.canonicalText == "org.greenrobot.eventbus.EventBus"
-            && (uCallExpression.methodName == "post" || uCallExpression.methodName == "postSticky")
-        ) {
-            return uCallExpression
-        }
-    }
-    return null
+private fun UCallExpression.isPost() : Boolean {
+    return receiverType?.canonicalText == "org.greenrobot.eventbus.EventBus"
+            && (methodName == "post" || methodName == "postSticky")
 }
 
 private class PostLineMarkerInfo(
